bbee12c9fc8a112cdb7ef1221ed1d5c4b8ec5f49
ww <aquawill@msn.com>
2025-04-25 22:40:25 +0800

    change icon

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: __dirname + '/assets/icon.ico',
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
